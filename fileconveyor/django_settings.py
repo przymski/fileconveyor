97b68d435c899be345b5acaffb60c4359996ed5e
Piotr Szwarc <p.szwarc@kainos.com>
2016-12-06 10:06:11 +0100

    Updated django_setting.py

--- fileconveyor/django_settings.py
+++ fileconveyor/django_settings.py
@@ -1,17 +1,20 @@
 # Dummy settings for `django-storages`.
-MEDIA_URL=''
-MEDIA_ROOT=''
+MEDIA_URL = ''
+MEDIA_ROOT = ''
 # `backends/ftp.py`
-FTP_STORAGE_LOCATION=''
+FTP_STORAGE_LOCATION = ''
 # `backends/sftp.py`
-SFTP_STORAGE_HOST=''
+SFTP_STORAGE_HOST = ''
 # django-cumulus
-#CUMULUS['USERNAME'] = '';
-#CUMULUS['CUMULUS_API_KEY'] = '';
-#CUMULUS['CONTAINER'] = '';
-CUMULUS_API_KEY = '';
-CUMULUS_USERNAME = '';
-CUMULUS_CONTAINER = '';
+# CUMULUS['USERNAME'] = '';
+# CUMULUS['CUMULUS_API_KEY'] = '';
+# CUMULUS['CONTAINER'] = '';
+CUMULUS_API_KEY = ''
+CUMULUS_USERNAME = ''
+CUMULUS_CONTAINER = ''
 
-# SECRET_KEY is required for amazon s3 storage provider boto library
-SECRET_KEY = 'vETf-M2PJq9T';
+# SECRET_KEY must be present
+# To generate visit: http://www.miniwebtool.com/django-secret-key-generator/
+# Doc: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+
+# SECRET_KEY='YOUR_KEY';
